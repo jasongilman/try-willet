82cd325e0f93b7d5c1d83e060b86a48731b587ec
Jason Gilman <jason@element84.com>
2020-07-07 13:00:02 -0400

    Adding bootstrap file.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,3 +1,27 @@
+#!/usr/bin/env bash
+
+git submodule init
+if [ $? != 0 ]; then
+  printf "Initialized submodule failure"
+  exit 1
+fi
+
+git submodule update
+if [ $? != 0 ]; then
+  printf "submodule update failure"
+  exit 1
+fi
+
 cd ace
+
 npm install
+if [ $? != 0 ]; then
+  printf "npm install failure"
+  exit 1
+fi
+
 node Makefile.dryice.js full --target ../ace-builds
+if [ $? != 0 ]; then
+  printf "ace build failure"
+  exit 1
+fi
